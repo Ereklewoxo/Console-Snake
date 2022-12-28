fa95cdf44d8e5a2398812878fc222eb3b156deb4
Erekle woxo <choxonelidze7@gmail.com>
2022-12-28 04:53:35 +0400

    Added Themes

--- Console Snake/Program.cs
+++ Console Snake/Program.cs
@@ -20,11 +20,71 @@ namespace Console_Snake
     }
     public class Title
     {
-        public static string Logo = "     CONSOLE" +
+        public static string Logo = "" +
+                  "     CONSOLE" +
                 "\n     ██▀▀█ ██  █ ▄█▀█▄ █   █ █▀▀▀▀" +
                 "\n      ▀█▄  █▀█▄█ █   █ █▄▄█▀ ████" +
-                "\n     █▄▄██ █  ██ █   █ █  ▀█ █▄▄▄▄" +
-              "\n\n                >START<";
+                "\n     █▄▄██ █  ██ █   █ █  ▀█ █▄▄▄▄\n\n";
+    }
+    public class Menu
+    {
+        public static List<Tuple<string, string>> ThemeColors()
+        {
+            var colors = new List<Tuple<string, string>>();
+            colors.Add(new Tuple<string, string>("\x1b[38;2;0;0;0m", "\x1b[48;2;255;255;255m"));
+            colors.Add(new Tuple<string, string>("\x1b[38;2;32;55;0m", "\x1b[48;2;142;222;8m"));
+            colors.Add(new Tuple<string, string>("\x1b[38;2;250;0;0m", "\x1b[48;2;10;0;0m"));
+            colors.Add(new Tuple<string, string>("\x1b[38;2;229;235;7m", "\x1b[48;2;40;45;240m"));
+            colors.Add(new Tuple<string, string>("\x1b[38;2;58;33;3m", "\x1b[48;2;243;240;195m"));
+            return colors;
+        }
+        public static int Theme = 0;
+        public static int MenuTheme()
+        {
+            ConsoleKeyInfo key;
+            int selected = 0;
+            do
+            {
+                if (selected == 0)
+                {
+                    Console.WriteLine("" +
+                        "                >START<\n" +
+                        "                 THEME ");
+                }
+                else if (selected == 1)
+                {
+                    Console.WriteLine("" +
+                        "                 START \n" +
+                        "                <THEME>");
+
+                }
+                key = Console.ReadKey(true);
+                if (key.Key == ConsoleKey.DownArrow)
+                    selected = 1;
+                else if (key.Key == ConsoleKey.UpArrow)
+                    selected = 0;
+                if (selected == 1)
+                {
+                    switch (key.Key)
+                    {
+                        case ConsoleKey.LeftArrow:
+                            Theme = Math.Max(Theme - 1, 0);
+                            Console.WriteLine(ThemeColors()[Theme].Item1 + ThemeColors()[Theme].Item2);
+                            Console.Clear();
+                            Console.WriteLine(Title.Logo + "\n\n");
+                            break;
+                        case ConsoleKey.RightArrow:
+                            Theme = Math.Min(Theme + 1, ThemeColors().Count - 1);
+                            Console.WriteLine(ThemeColors()[Theme].Item1 + ThemeColors()[Theme].Item2);
+                            Console.Clear();
+                            Console.WriteLine(Title.Logo + "\n\n");
+                            break;
+                    }
+                }
+                Console.SetCursorPosition(0, Console.CursorTop - 2);
+            } while (key.Key != ConsoleKey.Enter);
+            return Theme;
+        }
     }
     public class Game
     {
@@ -50,7 +110,7 @@ namespace Console_Snake
                     Console.WriteLine($" SCORE: {score}              SPEED: 1px/{speed}ms ");
                 else
                     Console.WriteLine($" SCORE: {score}                  SPEED: MAX ");
-                Console.Write("\x1b[38;2;0;0;0m\x1b[48;2;255;255;255m");
+                Console.Write(Menu.ThemeColors()[Menu.Theme].Item1 + Menu.ThemeColors()[Menu.Theme].Item2);
                 if (noFruit)
                 {
                     do
@@ -159,15 +219,12 @@ namespace Console_Snake
             Console.WindowHeight = 21;
             Console.WindowWidth = 40;
             Console.CursorVisible = false;
-            Console.WriteLine("\x1b[38;2;0;0;0m\x1b[48;2;255;255;255m");
+            Console.Write(Menu.ThemeColors()[Menu.Theme].Item1 + Menu.ThemeColors()[Menu.Theme].Item2);
             Console.Clear();
             do
             {
                 Console.WriteLine(Title.Logo);
-                do
-                {
-                    key = Console.ReadKey(true);
-                } while (key.Key != ConsoleKey.Enter);
+                Menu.MenuTheme();
                 Console.Clear();
                 Game.GameLoop();
                 key = Console.ReadKey(true);
